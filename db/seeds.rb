1046222edbe66060c6c0530ade06b1623774d75e
theo rouland <theo.rouland@laposte.net>
2019-08-14 16:47:02 +0200

    good

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create({name: 'Esprit Chai', address: '16 rue Oberkampf', phone_number: '0678653423', category: 'chinese'})
+Restaurant.create({name: 'Pitaya', address: '18 rue Oberkampf', phone_number: '0767090967', category: 'chinese'})
+Restaurant.create({name: 'Kebab', address: '20 rue Oberkampf', phone_number: '0890095643', category: 'italian'})
+Restaurant.create({name: 'Salad Bar', address: '22 rue Oberkampf', phone_number: '0678904675', category: 'french'})
+Restaurant.create({name: 'Sandwiches', address: '25 rue Oberkampf', phone_number: '0774089359', category: 'belgian'})
